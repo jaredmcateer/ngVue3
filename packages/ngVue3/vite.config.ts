fc611e565dabcfdb30cd6f15b5f2c703994ca342
Jared McAteer <jared.mcateer@gmail.com>
2022-05-02 08:38:06 -0600

    chore: rename ngvue folder

--- packages/ngVue3/vite.config.ts
+++ packages/ngVue3/vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import {resolve} from "path";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: resolve(__dirname, "lib/main.js"),
+      entry: resolve(__dirname, "lib/main.ts"),
       name: "ngVue3",
       fileName: (format) => `ngvue3.${format}.js`,
     },

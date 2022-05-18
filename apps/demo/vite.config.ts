e0e606ab4f94335090d22f1b32ed57952d2bab6f
Jared McAteer <jared.mcateer@gmail.com>
2022-05-18 15:07:10 -0600

    fix(demo): adding back in copy, symlink doesn't appear to be building on gh actions

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -1,8 +1,30 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import fs from "fs";
 
 const resolvePath = (path: string) => resolve(__dirname, path);
+const log = (msg: string) => console.log("\x1b[36m%s\x1b[0m", msg);
+
+function copyFileTo(file: string, dir: string) {
+  const from = resolvePath(dir);
+  const to = resolvePath(`dist/${dir}`);
+  return {
+    name: "copy-node-module-files",
+    generateBundle() {
+      if (!fs.existsSync(to)) {
+        log(`making directory: ${to}`);
+        fs.mkdirSync(to, { recursive: true });
+      }
+
+      const src = `${from}/${file}`;
+      const dst = `${to}/${file}`;
+
+      log(`copy files: ${src} -> ${dst}`);
+      fs.copyFileSync(src, dst);
+    },
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,6 +42,11 @@ export default defineConfig({
         setupExample: resolvePath("examples/script-setup/index.html"),
         umdExample: resolvePath("examples/umd/index.html"),
       },
+      plugins: [
+        copyFileTo("main.js", "node_modules/@jaredmcateer/ngvue3/dist/"),
+        copyFileTo("angular.min.js", "node_modules/angular/"),
+        copyFileTo("vue.global.js", "node_modules/vue/dist/"),
+      ],
     },
   },
 });

27b0dc521b044b20402bc5404a81cdefd3d3f593
Jared McAteer <jared.mcateer@gmail.com>
2022-05-18 14:25:52 -0600

    fix(demo): copy node modules files for umd demo

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import fs from "fs";
 
+const resolvePath = (path: string) => resolve(__dirname, path);
+const log = (msg: string) => console.log("\x1b[36m%s\x1b[0m", msg);
+
+function copyFileTo(file: string, dir: string) {
+  const from = resolvePath(dir);
+  const to = resolvePath(`dist/${dir}`);
+  return {
+    name: "copy-node-module-files",
+    generateBundle() {
+      if (!fs.existsSync(to)) {
+        log(`making directory: ${to}`);
+        fs.mkdirSync(to, { recursive: true });
+      }
+
+      const src = `${from}/${file}`;
+      const dst = `${to}/${file}`;
+
+      log(`copy files: ${src} -> ${dst}`);
+      fs.copyFileSync(src, dst);
+    },
+  };
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -12,12 +35,17 @@ export default defineConfig({
     minify: false,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        compositionExample: resolve(__dirname, "examples/composition-api/index.html"),
-        optionsExample: resolve(__dirname, "examples/options-api/index.html"),
-        setupExample: resolve(__dirname, "examples/script-setup/index.html"),
-        umdExample: resolve(__dirname, "examples/umd/index.html"),
+        main: resolvePath("index.html"),
+        compositionExample: resolvePath("examples/composition-api/index.html"),
+        optionsExample: resolvePath("examples/options-api/index.html"),
+        setupExample: resolvePath("examples/script-setup/index.html"),
+        umdExample: resolvePath("examples/umd/index.html"),
       },
+      plugins: [
+        copyFileTo("main.js", "node_modules/@jaredmcateer/ngvue3/dist/"),
+        copyFileTo("angular.min.js", "node_modules/angular/"),
+        copyFileTo("vue.global.js", "node_modules/vue/dist/"),
+      ],
     },
   },
 });

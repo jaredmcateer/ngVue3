153507bc244018bfbfb3c5a592829fdc3d3339f5
Jared McAteer <jared.mcateer@gmail.com>
2022-05-18 12:52:04 -0600

    fix(build): build not output umd package
    Reverted back to vite to build (originally switch to tsup for being able to build without
    minification)

--- packages/ngVue3/vite.config.ts
+++ packages/ngVue3/vite.config.ts
@@ -1,15 +1,28 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import typescriptPlugin from "@rollup/plugin-typescript";
 import { resolve } from "path";
+
+const resolvePath = (str: string) => resolve(__dirname, str);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
     lib: {
-      entry: resolve(__dirname, "lib/main.ts"),
+      entry: resolvePath("lib/main.ts"),
       name: "ngVue3",
-      fileName: (format) => `ngvue3.${format}.js`,
+      formats: ["es", "umd", "cjs"],
+      fileName: (format) => {
+        switch (format) {
+          case "umd":
+            return `main.js`;
+          case "es":
+            return `main.mjs`;
+          case "cjs":
+            return `main.cjs`;
+        }
+      },
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
@@ -19,10 +32,22 @@ export default defineConfig({
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
+          ngVue3: "ngVue3",
           vue: "Vue",
           angular: "angular",
         },
+        sourcemap: true,
       },
+      plugins: [
+        typescriptPlugin({
+          tsconfig: resolvePath("tsconfig.json"),
+          declaration: true,
+          declarationDir: resolvePath("dist"),
+          exclude: resolvePath("node_modules/**"),
+          allowSyntheticDefaultImports: true,
+          sourceMap: true,
+        }),
+      ],
     },
   },
 });
